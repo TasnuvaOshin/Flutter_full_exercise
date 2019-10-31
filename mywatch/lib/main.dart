65c6da4dc9fab8987c823c38ddd5d71303aa522b
Tasnuva Tabassum Oshin <tasnuva.oshin12@gmail.com>
2019-10-31 16:15:17 +0600

    How to do Tab Works in Flutter

---
+++ mywatch/lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Watch',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Firstpage(),
+    );
+  }
+}
+
+class Firstpage extends StatefulWidget {
+  @override
+  _FirstpageState createState() => _FirstpageState();
+}
+
+class _FirstpageState extends State<Firstpage> with TickerProviderStateMixin{
+
+TabController tabController;
+
+      @override
+      void initState(){
+
+        tabController = TabController(
+          length: 2,
+          vsync: this,
+        );
+
+        super.initState();
+      }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar: AppBar(
+
+        title: Text(
+          "Home"
+        ),
+         bottom: TabBar(
+
+          tabs: <Widget>[
+
+          Text("Time"),
+          Text("Stop watch"),
+
+          ],
+controller: tabController,
+       ),
+        backgroundColor: Colors.deepPurpleAccent,
+      ),
+    
+    );
+  }
+}
+
+
