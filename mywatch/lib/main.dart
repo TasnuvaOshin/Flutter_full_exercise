4b7bf7ff69953c3b65a69b3b569222f4a9aa3544
Tasnuva Tabassum Oshin <tasnuva.oshin12@gmail.com>
2019-11-19 11:29:22 +0600

    added something new

--- mywatch/lib/main.dart
+++ mywatch/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:numberpicker/numberpicker.dart';
 
 void main() => runApp(MyApp());
 
@@ -24,6 +25,7 @@ class Firstpage extends StatefulWidget {
 class _FirstpageState extends State<Firstpage> with TickerProviderStateMixin{
 
 TabController tabController;
+int hour = 0;
 
       @override
       void initState(){
@@ -46,34 +48,242 @@ TabController tabController;
         title: Text(
           "Home"
         ),
+        centerTitle: true,
          bottom: TabBar(
 
           tabs: <Widget>[
 
-          Text("Time"),
+          Text("Timer"),
           Text("Stop watch"),
 
           ],
+
+          labelStyle: TextStyle(
+
+            fontSize: 15.0,
+            fontWeight: FontWeight.bold
+          
+          ),
+          labelPadding: EdgeInsets.only(
+           
+                  bottom: 20.0,
+          ),
+          unselectedLabelColor: Colors.white70,
           controller: tabController,
        ),
         backgroundColor: Colors.deepPurpleAccent,
       ),
     
     body: TabBarView(
+//changes something new 
 
       children: <Widget>[
+                         //we are calling this widget for showing the timer 
+                      timerpage(),
+                      Text("Stop watch"),
+                                 
+                                 
+                                       ],
+                                     controller: tabController,
+                                     ),
+                                     
+                                     );
+                                   }
+           
+          Widget  timerpage() {
+            return Container(
+              child: Column(
+               children: <Widget>[
+                   Expanded(
+                      
+                     flex: 6,
+                     child: Row(
+
+                       children: <Widget>[
+                              Column(
+
+            children: <Widget>[
+             
+
+            Padding(
+
+              padding: EdgeInsets.only(
+
+                   bottom: 20.5,
+
+              ),
+                  child: Text(
+                    "HH",
+                  ),
+                        
+            ),
+
+            NumberPicker.integer(
 
 
-          Text("Time"),
-          Text("Stop watch"),
+                 initialValue: hour,
+                 maxValue: 23,
+                 minValue: 0,
+                  onChanged: (val){
 
 
-      ],
-    controller: tabController,
-    ),
-    
-    );
-  }
+                    setState(() {
+                      hour = val;
+
+
+                    });
+                  },
+
+                  )
+
+            ],
+
+   ),
+                  
+                  
+         Column(
+
+            children: <Widget>[
+             
+
+            Padding(
+
+              padding: EdgeInsets.only(
+
+                   bottom: 20.5,
+
+              ),
+                  child: Text(
+                    "HH",
+                  ),
+                        
+            ),
+
+            NumberPicker.integer(
+
+
+                 initialValue: hour,
+                 maxValue: 23,
+                 minValue: 0,
+                  onChanged: (val){
+
+
+                    setState(() {
+                      hour = val;
+
+
+                    });
+                  },
+
+                  )
+
+            ],
+
+   ),
+   Column(
+
+            children: <Widget>[
+             
+
+            Padding(
+
+              padding: EdgeInsets.only(
+
+                   bottom: 20.5,
+
+              ),
+                  child: Text(
+                    "HH",
+                  ),
+                        
+            ),
+
+            NumberPicker.integer(
+
+
+                 initialValue: hour,
+                 maxValue: 23,
+                 minValue: 0,
+                  onChanged: (val){
+
+
+                    setState(() {
+                      hour = val;
+
+
+                    });
+                  },
+
+                  )
+
+            ],
+
+   ),         
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                      ],
+                     ),
+                   ),
+
+                     Expanded(
+                      
+                     flex: 1,
+                     child: Text(
+
+                       "My Demo Dmeo"
+                     ),
+
+                     
+                   ),
+                     Expanded(
+                      
+                     flex: 3,
+                     child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        
+         RaisedButton(
+
+           onPressed: (){},
+          child: Text(
+            "Run Timer"
+          ),
+         ),
+
+          RaisedButton(
+
+           onPressed: (){},
+          child: Text(
+            "Stop Timer"
+          ),
+         ),
+                      ],
+                     ),
+
+                      
+                   ),
+
+
+
+               ],
+              ),                     
+                     
+                  
+               
+            
+               
+              
+
+
+            );
+          }
+ 
+   
 }
 
 
